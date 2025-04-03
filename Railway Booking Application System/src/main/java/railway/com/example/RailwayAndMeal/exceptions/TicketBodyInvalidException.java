be092640193b93c6268a7b9cac8689994dad6d94
LOGESH KANNAN M <109458859+logeshkannanmuniaraj@users.noreply.github.com>
2025-04-03 17:46:45 +0530

    Add files via upload

---
+++ Railway Booking Application System/src/main/java/railway/com/example/RailwayAndMeal/exceptions/TicketBodyInvalidException.java
@@ -0,0 +1,13 @@
+package railway.com.example.RailwayAndMeal.exceptions;
+
+/**
+     1. Add proper annotation for this custom exception class
+     2. It returns a response status of type BAD_REQUEST.
+ */
+public class TicketBodyInvalidException extends RuntimeException {
+
+    //complete the method body for TicketBodyInvalidException constructor
+    public TicketBodyInvalidException(String message) {
+
+    }
+}
